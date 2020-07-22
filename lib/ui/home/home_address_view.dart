35016fe11e731a6c98e55ececb68e0cb5421b6ab
Shubham Sharma <shubhamsharma1172@gmail.com>
2020-07-22 21:53:44 +0530

    add store view
    Signed-off-by: Shubham Sharma <shubhamsharma1172@gmail.com>

---
+++ lib/ui/home/home_address_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeAddressView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        text: TextSpan(
+            style: GoogleFonts.openSans(fontSize: 16, color: Color(0XFF99879D)),
+            children: <TextSpan>[
+          TextSpan(
+              text:
+                  "Showing stores near your address - Microsoft Signature Bldg,... Bengaluru 560071. "),
+          TextSpan(
+              text: "Change?",
+              style: TextStyle(decoration: TextDecoration.underline))
+        ]));
+  }
+}
